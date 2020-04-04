5ff716c251c2db8730b371f73d4ec27cdd1a85c0
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-04 22:23:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
