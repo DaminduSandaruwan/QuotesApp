2c434cc989769c74425939e6876256d798727b85
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-05 18:18:31 +0530

    Card - add card template using function

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,37 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'Oscar Wilde', text:'Be yourself, everyone else already taken'),
     Quote(author: 'Oscar Wilde', text:'I have nothing to declare expect my genius'),
     Quote(author: 'Oscar Wilde', text:'The truth is rarely pure and never simple')
-//    'Be yourself, everyone else already taken',
-//    'I have nothing to declare expect my genius',
-//    'The truth is rarely pure and never simple'
+
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
 
   @override
@@ -32,10 +59,12 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
+         children: quotes.map((quote)=> quoteTemplate(quote)).toList(),
+
 //        children: quotes.map((quote) => Text(quote)).toList(),
-        children: quotes.map((quote){
-          return Text('${quote.text} - ${quote.author}');
-        }).toList(),
+//        children: quotes.map((quote){
+//          return Text('${quote.text} - ${quote.author}');
+//        }).toList(),
       ),
     );
   }
